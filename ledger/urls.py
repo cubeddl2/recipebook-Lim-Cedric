668f66c2cb79d8d29498bb3b402927b5ec84bc80
CubeddL2 <skewbiscool@gmail.com>
2026-02-09 22:16:53 +0800

    Fixed hardcoded recipe paths in urls.py

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,8 +5,7 @@ from .views import *
 urlpatterns = {
     path('', index, name="index"),
     path('recipes/list', recipes_list, name='recipes'),
-    path('recipe/1', recipe_1, name='recipe_1'),
-    path('recipe/2', recipe_2, name='recipe_2'),
+    path('recipe/<int:id>', recipe, name='recipe'),
 }
 
 app_name="ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -71,68 +71,68 @@ def recipes_list(request):
     }
     return render(request, "ledger/recipes_list.html", ctx)
 
-def recipe_1(request):
+def recipe(request, id):
+    
+    if id == 1:
         ctx =   {
-                    "name": "Recipe 1",
-                    "ingredients": [
-                        {
-                            "name": "tomato",
-                            "quantity": "3pcs"
-                        },
-                        {
-                            "name": "onion",
-                            "quantity": "1pc"
-                        },
-                        {
-                            "name": "pork",
-                            "quantity": "1kg"
-                        },
-                        {
-                            "name": "water",
-                            "quantity": "1L"
-                        },
-                        {
-                            "name": "sinigang mix",
-                            "quantity": "1 packet"
-                        }
-                    ],
-                    "link": "/recipe/1"
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
                 }
-        return render(request, "ledger/recipe.html", ctx)
-
-def recipe_2(request):
+            ],
+            "link": "/recipe/1"
+        }
+    elif id == 2:
         ctx =   {
-                    "name": "Recipe 2",
-                    "ingredients": [
-                        {
-                            "name": "garlic",
-                            "quantity": "1 head"
-                        },
-                        {
-                            "name": "onion",
-                            "quantity": "1pc"
-                        },
-                        {
-                            "name": "vinegar",
-                            "quantity": "1/2cup"
-                        },
-                        {
-                            "name": "water",
-                            "quantity": "1 cup"
-                        },
-                        {
-                            "name": "salt",
-                            "quantity": "1 tablespoon"
-                        },
-                        {
-                            "name": "whole black peppers",
-                            "quantity": "1 tablespoon"
-                        },
-                        {
-                            "name": "pork",
-                            "quantity": "1 kilo"
-                        }
-                    ],
-                    "link": "/recipe/2"
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
                 }
-        return render(request, "ledger/recipe.html", ctx)
+            ],
+            "link": "/recipe/2"
+        }
+    return render(request, "ledger/recipe.html", ctx)
